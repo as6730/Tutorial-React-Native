bb0db566ce84931e1a35e81d145cf4634c2b0c9e
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-05-20 14:13:49 -0700

    Create and style header component

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
 
+import React from 'react';
+import { AppRegistry } from 'react-native'
+import Header from './src/components/header';
+
+// Create a component 
+const App = () => (
+  <Header />
+);
+
+// Render it to the device 
+// Must always register at least one component 
 AppRegistry.registerComponent('albums', () => App);
+
+// AppRegistry.registerComponent('albums', () => App);
